6a8041c2d60a36e7f3f9f8e1ae87f2165a5e3644
up201905951 <up201905951@fe.up.pt>
2021-03-02 14:17:45 +0000

    all files tp3

---
+++ tp3/MyUnitCube.js
@@ -0,0 +1,41 @@
+import { CGFobject } from "../lib/CGF.js";
+
+export class MyUnitCube extends CGFobject {
+    constructor(scene){
+        super(scene);
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [
+        -0.5, 0.5,-0.5, //0
+        -0.5,-0.5,-0.5, //1
+         0.5,-0.5,-0.5, //2
+         0.5, 0.5,-0.5, //3
+        -0.5, 0.5, 0.5, //4
+        -0.5,-0.5, 0.5, //5
+         0.5,-0.5, 0.5, //6
+         0.5, 0.5, 0.5, //7
+        ];
+
+        this.indices = [
+            0,3,2,
+            1,0,2,
+            3,0,7,
+            0,4,7,
+            4,0,1,
+            1,5,4,
+            5,1,2,
+            2,6,5,
+            2,3,7,
+            4,7,3,
+            7,6,2,
+            6,7,4,
+            6,4,5
+        ];
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+
+        this.initGLBuffers();
+    }
+}
