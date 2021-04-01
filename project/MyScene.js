81a1cbc075ae55f59c52e258975e1fa75be99fae
up201905951 <up201905951@fe.up.pt>
2021-04-01 09:50:32 +0100

    console test W & S

--- project/MyScene.js
+++ project/MyScene.js
@@ -71,6 +71,26 @@ export class MyScene extends CGFscene {
     // called periodically (as per setUpdatePeriod() in init())
     update(t){
         //To be done...
+        this.checkKeys();
+    }
+
+    checkKeys() {
+        var text="Keys pressed: ";
+        var keysPressed=false;
+
+        // Check for key codes e.g. in https://keycode.info/
+        if (this.gui.isKeyPressed("KeyW")) {
+            text+=" W ";
+            keysPressed=true;
+        }
+
+        if (this.gui.isKeyPressed("KeyS"))        {
+            text+=" S ";
+            keysPressed=true;
+        }
+
+        if (keysPressed)
+            console.log(text);
     }
 
     display() {
