daa988240438e16c72dca5a6cbca781c0f57616c
Tiago Rodrigues <up201907021@fe.up.pt>
2021-04-07 11:34:38 +0100

    Changes properties of textures to look brighter and more realistic

--- project/MyUnitCubeQuad.js
+++ project/MyUnitCubeQuad.js
@@ -18,50 +18,56 @@ export class MyUnitCubeQuad extends CGFobject {
         this.texture6 = new CGFtexture(this.scene, "images/demo_cubemap/front.png");
 
         this.topMaterial = new CGFappearance(this.scene);
-        this.topMaterial.setAmbient(0.1, 0.1, 0.1, 1);
-        this.topMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.topMaterial.setSpecular(0.1, 0.1, 0.1, 1);
-        this.topMaterial.setShininess(10.0);
+        this.topMaterial.setAmbient(0.0, 0.0, 0.0, 0.0);
+        this.topMaterial.setDiffuse(0.0, 0.0, 0.0, 0.0);
+        this.topMaterial.setSpecular(0.0, 0.0, 0.0, 0.0);
+        this.topMaterial.setShininess(0.0);
+        this.topMaterial.setEmission(1, 1, 1, 1);
         this.topMaterial.setTexture(this.texture5);
         this.topMaterial.setTextureWrap('REPEAT', 'REPEAT');
 
         this.sideMaterial1 = new CGFappearance(this.scene);
-        this.sideMaterial1.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideMaterial1.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideMaterial1.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideMaterial1.setShininess(10.0);
+        this.sideMaterial1.setAmbient(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial1.setDiffuse(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial1.setSpecular(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial1.setShininess(0.0);
+        this.sideMaterial1.setEmission(1, 1, 1, 1);
         this.sideMaterial1.setTexture(this.texture1);
         this.sideMaterial1.setTextureWrap('REPEAT', 'REPEAT');
 
         this.sideMaterial2 = new CGFappearance(this.scene);
-        this.sideMaterial2.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideMaterial2.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideMaterial2.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideMaterial2.setShininess(10.0);
+        this.sideMaterial2.setAmbient(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial2.setDiffuse(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial2.setSpecular(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial2.setShininess(0.0);
+        this.sideMaterial2.setEmission(1, 1, 1, 1);
         this.sideMaterial2.setTexture(this.texture3);
         this.sideMaterial2.setTextureWrap('REPEAT', 'REPEAT');
 
         this.sideMaterial3 = new CGFappearance(this.scene);
-        this.sideMaterial3.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideMaterial3.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideMaterial3.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideMaterial3.setShininess(10.0);
+        this.sideMaterial3.setAmbient(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial3.setDiffuse(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial3.setSpecular(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial3.setShininess(0.0);
+        this.sideMaterial3.setEmission(1, 1, 1, 1);
         this.sideMaterial3.setTexture(this.texture4);
         this.sideMaterial3.setTextureWrap('REPEAT', 'REPEAT');
 
         this.sideMaterial4 = new CGFappearance(this.scene);
-        this.sideMaterial4.setAmbient(0.1, 0.1, 0.1, 1);
-        this.sideMaterial4.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.sideMaterial4.setSpecular(0.1, 0.1, 0.1, 1);
-        this.sideMaterial4.setShininess(10.0);
+        this.sideMaterial4.setAmbient(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial4.setDiffuse(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial4.setSpecular(0.0, 0.0, 0.0, 0.0);
+        this.sideMaterial4.setShininess(0.0);
+        this.sideMaterial4.setEmission(1, 1, 1, 1);
         this.sideMaterial4.setTexture(this.texture6);
         this.sideMaterial4.setTextureWrap('REPEAT', 'REPEAT');
 
         this.bottomMaterial = new CGFappearance(this.scene);
-        this.bottomMaterial.setAmbient(0.1, 0.1, 0.1, 1);
-        this.bottomMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.bottomMaterial.setSpecular(0.1, 0.1, 0.1, 1);
-        this.bottomMaterial.setShininess(10.0);
+        this.bottomMaterial.setAmbient(0.0, 0.0, 0.0, 0.0);
+        this.bottomMaterial.setDiffuse(0.0, 0.0, 0.0, 0.0);
+        this.bottomMaterial.setSpecular(0.0, 0.0, 0.0, 0.0);
+        this.bottomMaterial.setShininess(0.0);
+        this.bottomMaterial.setEmission(1, 1, 1, 1);
         this.bottomMaterial.setTexture(this.texture2);
         this.bottomMaterial.setTextureWrap('REPEAT', 'REPEAT');
     }
@@ -71,7 +77,7 @@ export class MyUnitCubeQuad extends CGFobject {
         this.scene.pushMatrix();
         
         //camara position
-        //this.scene.translate(15,15,15);
+        this.scene.translate(15,15,15);
         this.scene.scale(50,50,50);
 
         this.scene.pushMatrix();
