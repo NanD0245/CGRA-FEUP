1198194a0eb7d3332a1da191ca86a1888fc3f7ba
Tiago Rodrigues <up201907021@fe.up.pt>
2021-04-07 10:27:17 +0100

    Added MyUnitCube Quad

---
+++ project/MyUnitCubeQuad.js
@@ -0,0 +1,47 @@
+import { CGFobject } from '../lib/CGF.js';
+import { MyQuad } from './MyQuad.js';
+
+export class MyUnitCubeQuad extends CGFobject {
+    constructor(scene){
+        super(scene);
+        this.face = new MyQuad(this.scene);
+    }
+
+    display() {
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
+        this.face.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, -0.5);
+        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.face.display();
+        this.scene.popMatrix();
+        
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0.5, 0);
+        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.face.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, -0.5, 0);
+        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.face.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, 0, 0);
+        this.scene.rotate(Math.PI/2, 0, 1, 0);
+        this.face.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 0, 0);
+        this.scene.rotate(-Math.PI/2, 0, 1, 0);
+        this.face.display();
+        this.scene.popMatrix();
+    }
+}
