b0be9302313655035ea2e8b3113a1076ebdf0a0a
Tiago Rodrigues <up201907021@fe.up.pt>
2021-05-06 11:48:03 +0100

    Small code cleanup

--- project/assets/moving_object/MyAutomatedFish.js
+++ project/assets/moving_object/MyAutomatedFish.js
@@ -1,30 +1,32 @@
 import { MyMovingFish } from "./MyMovingFish.js";
 
-
 export class MyAutomatedFish extends MyMovingFish {
-    constructor(scene) {
-        super(scene);
-        this.center = [Math.floor(Math.random() * 99) - 49, Math.floor(Math.random() * 5) + 1,Math.floor(Math.random() * 99) - 49];
-        this.velocity = 0.1;
-        this.orientationYY = Math.floor(Math.random()) < 0.5 ? -1 : 1;
-        this.orientationYY *= Math.PI/ 36;
-        this.initial = this.orientationYY;
-        
-        this.initPosition();
-    }
+  constructor(scene) {
+    super(scene);
+    this.center = [
+      Math.floor(Math.random() * 99) - 49,
+      Math.floor(Math.random() * 5) + 1,
+      Math.floor(Math.random() * 99) - 49,
+    ];
+    this.velocity = 0.1;
+    this.orientationYY = Math.floor(Math.random()) < 0.5 ? -1 : 1;
+    this.orientationYY *= Math.PI / 36;
+    this.initial = this.orientationYY;
 
-    initPosition() {
-        this.position = [this.center[0] + 5, this.center[1], this.center[2]];
-    }
+    this.initPosition();
+  }
 
-    update() { 
-        if (this.initial <= 0) {
-            this.pyramid.turningRight = true;
-        } else {
-            this.pyramid.turningLeft = true;
-        }
-        super.update();
-        this.orientationYY += this.initial % 7;
-    }
+  initPosition() {
+    this.position = [this.center[0] + 5, this.center[1], this.center[2]];
+  }
 
+  update() {
+    if (this.initial <= 0) {
+      this.pyramid.turningRight = true;
+    } else {
+      this.pyramid.turningLeft = true;
+    }
+    super.update();
+    this.orientationYY += this.initial % 7;
+  }
 }
